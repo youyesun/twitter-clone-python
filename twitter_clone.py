8ed51f3228c72d492f2fb8cd17315c05f88f91eb
Youye <sun.yo@husky.neu.edu>
2015-08-11 00:49:01 -0400

    add isLoggedIn

--- twitter_clone.py
+++ twitter_clone.py
@@ -1,6 +1,17 @@
 #! /usr/bin/python
 
-import redis
+import redis, md5
+from flask import request
+
+
+def getrand():
+    with open("/dev/urandom", "r") as fd:
+        data = fd.readline()
+        m = md5.new()
+        m.update(data)
+        return m.digest()
+ 
+
 
 def static_vars(**kwargs):
     def decorate(func):
@@ -16,5 +27,40 @@ def redisLink():
         return redisLink.r
     redisLink.r = redis.StrictRedis(host='localhost', port=6379, db=0)
     return redisLink.r
+
+
+def isLoggedIn():
+    global User
+    try:
+        User
+    except NameError:
+        try:
+            request.cookies['auth']
+        except KeyError:
+            return False
+        else:
+            r = redisLink()
+            authcookie = request.cookies['auth']
+            userid = r.hget('auths', authcookie)
+            if userid:
+                if r.hget('user:'+str(userid), "auth") != authcookie:
+                    return False
+                loadUserInfo(userid)
+                return True      
+    else:
+        return True
+
+
+def loadUserInfo(userid):
+    global User
+    r = redisLink()
+    User = dict()
+    User['id'] = userid
+    User['username'] = r.hget('user:'+str(userid), 'username')
+    return True
+
+
+
+
 
 
